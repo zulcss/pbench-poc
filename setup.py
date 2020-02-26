2565c320cccc9b598899c5c23718a57cfbf70d4b
Charles Short <chucks@redhat.com>
2020-02-26 17:52:57 -0500

    initial commit
    Signed-off-by: Charles Short <chucks@redhat.com>

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="pbench",
+    description="A Benchmarking and Performance Analysis Framework",
+    url="https://github.com/distributed-system-analysis/pbench",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={}
+)
